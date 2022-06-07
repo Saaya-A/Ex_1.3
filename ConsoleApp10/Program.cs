2218146363fb6eecc4092133980b2ea87e956064
saydi <saydi@ENGINEER>
2022-06-07 18:29:42 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int kol = 0;
+        int[] a = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = r.Next(100);
+            Console.Write(" " + a[i]);
+        }
+        Console.WriteLine();
+        for (int i = 1; i < a.Length; i++)
+        {
+            if (2 * a[i - 1] < a[i])
+                kol++;
+        }
+        Console.Write("Количество чисел, соседи которых отличаются более чем в два раза " + kol);
+    }
+}
